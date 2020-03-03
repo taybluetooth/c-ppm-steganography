81e3b7967e632a2005e348bd538ad2d335c75a3c
taybluetooth <callumtaylorbusiness@gmail.com>
2020-03-03 12:21:51 +0000

    Refactored structures, added in encodefile method and started implementing decoding function

--- steg.c
+++ steg.c
@@ -3,268 +3,352 @@
 #include <string.h>
 #include <time.h>
 
-/*
-  A structure that will hold an rgb pixel of
-  max value 255.
-*/
-
-typedef struct PIXEL {
-  unsigned int r, g, b;
+typedef struct PIXEL{
+	int r, g, b;
 } PIXEL;
 
-/*
-  A structure that holds the PPM header, outlining
-  all important information of the file.
-*/
+/* text struct holds strings of comments */
+typedef struct TEXT {
+	char * text;
+	struct text * next;
+} TEXT;
 
-typedef struct HEADER {
-  char format[3];
-  char comment;
-  int width;
-  int height;
-  int max;
-} HEADER;
+struct PPM {
+	char * format;
+	struct text * comment;
+	int cn;
+	int width, height, max;
+	struct pixel ** pixels;
+};
 
-/*
-  Combined structure of PPM file that holds both
-  the header and an array of all pixels in the file.
-*/
+char * readLine(FILE * f){
 
-typedef struct PPM {
-  HEADER header;
-  PIXEL** pixels;
-} PPM;
+	char buffer[255];
+	int i, j;
+	char character;
+	char * line;
 
-/*
-  Additional function which allocates memory corresponding
-  to the size of the completed header to the final PPM image,
-  then allocating each pixel into the array.
-*/
-
-PPM* mem_alloc(HEADER header){
-	int i = 0;
-	PPM* image = (PPM *)malloc(sizeof(PPM));
-
-	PIXEL** pixels = (PIXEL **)malloc(header.height * sizeof(PIXEL *));
-	for(i = 0; i < header.height; i++){
-		pixels[i] = (PIXEL *)malloc(header.width * sizeof(PIXEL));
+	i = 0;
+	character = getc(f);
+	if (character == EOF)
+		return NULL;
+	while (character !='\n' && i < (255 -1)){
+		buffer[i] = ch;
+		i++;
+		character = getc(f);
 	}
 
-	image->pixels = pixels;
-	return image;
+	if (character != '\n')
+		while (ch != '\n')
+			character = getc(f);
+	buffer[i] = '\0';
+	line = malloc((i+1) * sizeof(char));
+
+	for (j = 0; j <= i; k++)
+		line[i] = buffer[j];
+		line[i] = '\0';
+
+	return line;
+	free(buffer);
 }
 
-/*
-  Function which reads in the header of the PPM file and assigns it
-  to the pre-defined Header struct, returning said header.
-*/
 
-struct HEADER getHeader(FILE * f) {
-  struct HEADER header;
-  int c;
-  char buffer[18];
+/* showPPM prints information stored in PPM struct
+ * in the given structural format
+ */
+showPPM (struct PPM * im){
 
-  /*
-    Read in file format P3.
-  */
+  //print format
+  printf("%s\n", im->format);
 
-  if (!fgets(buffer, sizeof(buffer), f)) {
-    exit(1);
+  //print text
+  int count;
+  struct text * text = im->comment;
+  printf("%s\n", im->comment->text);
+  for(j = 1; j<i->cn; j++){
+          text = text->next;
+          printf("%s\n", text->text);
   }
 
-  /*
-    Ignore all comments that start with '#'.
-  */
+  //print general info
+  printf("%d\n", im->width);
+  printf("%d\n", im->height);
+  printf("%d\n", im->max);
 
-  c = getc(f);
-  while (c == '#') {
-  while (getc(f) != '\n') ;
-    c = getc(f);
-  }
-
-  /*
-    Once line ends with no more #, unassign the character and continue
-    reading file.
-  */
-
-  ungetc(c, f);
-
-  /*
-    Read in header information of PPM file.
-  */
-
-  fscanf(f, "%i %i %i", &header.width, &header.height, &header.max);
-
-  return header;
-}
-
-/*
-  Method which gets all pixels from the PPM file, and within a loop, allocates
-  each pixel to an index of the pixel array, while utilising the memory allocation
-  function I created.
-*/
-
-PPM* getPixels(FILE* f, HEADER header) {
-  int i = 0;
-	int j = 0;
-  PPM* image = mem_alloc(header); //calls the allocation function
-	memcpy(&(image->header), &header, sizeof(header)); //populate the image pointer with the header data.
-
-  for(i = 0; i < header.height; i++){
-		for(j = 0; j < header.width; j++){
-			fscanf(f, "%u", &image->pixels[i][j].r);
-			fscanf(f, "%u", &image->pixels[i][j].g);
-			fscanf(f, "%u", &image->pixels[i][j].b);
-		};
-	};
-
-	return image;
-};
-
-/*
-  Method which returns the fully completed PPM structure after calling
-  both helper functions.
-*/
-
-struct PPM getPPM(FILE * f) {
-
-  struct PPM *ppm;
-
-  HEADER header = getHeader(f);
-	PPM* image = getPixels(f, header);
-
-  ppm = image;
-
-  fprintf(stderr, "%s\n", "PPM Structure Successfully Populated");
-
-	return *ppm; //image pointer return
-
-};
-
-/*
-  Method which returns the PPM file in a text format,
-  storing it in a text file for easier viewing and analysis.
-*/
-
-int showPPM(struct PPM * im) {
-
-  int i = 0;
-  int j = 0;
-  HEADER header = im -> header;
-
-  fprintf(stderr, "P3\n");
-  fprintf(stderr, "%i %i\n%i\n", header.width, header.height, header.max);
-
-  for(i = 0; i < header.height; i++){
-		for(j = 0; j < header.width; j++){
-			fprintf(stderr,"%u %u %u\n", im->pixels[i][j].r,  im->pixels[i][j].g,  im->pixels[i][j].b);
-		};
-	};
-
-  fprintf(stderr, "%s\n", (char *) "Successfully Converted PPM to Text");
-
-  return 0;
-
-};
-
-void writePPM(FILE * f, struct PPM * im){
-	FILE *output = NULL;
-	int i,j;
-
-	output = fopen("ppm-conversion", "w");
-	fprintf(output, "P3\n%d %d\n255\n", im->header.width, im->header.height);
-
-	for(i=0; i<im->header.height; i++) {
-		for(j=0; j<im->header.width;j++){
-			fprintf(output, "%i %i %i \n", im->pixels[i][j].r, im->pixels[i][j].g, im->pixels[i][j].b);
+  //print pixels
+  int i, j;
+  for(i = 0; i < im -> height; i++){
+    for(j = 0; j < im -> width; j++){
+      struct pixel p = im->pixels[i][j];
+      printf("%i %i %i\n", p.r, p.g, p.b);
     }
+  }
+}
+
+
+/* Utilizes the above readLine function to find the comments */
+void getText(FILE * f, struct PPM * im){
+	char character = getc(f);
+	while(character == '#') //lines beginning with "#" character
+	{
+		ungetc(character, f);
+
+		//get comment
+		char * line = readLine(f);
+
+		//add comment to linked list
+		if(im->cn == 0) {
+			im->comment = (struct text * )malloc(sizeof(struct text));
+			im->comment->text = line;
+			im->cn = 1;
+		}
+		else {
+			//previous comments
+			int i;
+			struct text * current = im->comment;
+
+			for(i = 1; i < im -> cn; i++) {
+  			current = current -> next;
+  			current -> next = (struct text *)malloc(sizeof(struct text));
+			  current -> next -> text = line;
+			  im -> cn = im->cn + 1;
+		}
+
+		character = getc(f);
 	}
-  fprintf(stderr, "%s\n", (char *) "Stored in File - ppm-conversion.txt");
+
+	//Restore to starting position
+	ungetc(character, f);
+}
+
+void getPixels(FILE * f, struct PPM * im){
+
+	//allocate rows
+	im-> pixels = (struct pixel ** ) malloc(sizeof(struct pixel *) * im -> height);
+
+	int i, j;
+	for(i = 0; i < im -> height; i++) {
+		//allocate columns
+		im -> pixels[j] = (struct pixel * ) malloc(sizeof(struct pixel) * im -> width);
+		for (j = 0; j < im -> width; j++) {
+			//get pixels for each
+			fscanf (f, "%d", &(im -> pixels[i][j].r)); //get memory address
+			fscanf (f, "%d", &(im -> pixels[i][j].g));
+			fscanf (f, "%d", &(im -> pixels[i][j].b));
+		}
+	}
+}
+
+
+/* Return PPM image file from fin */
+struct PPM * getPPM(FILE * f){
+  struct PPM * im = malloc(sizeof(struct PPM));
+
+  //Type
+  im->type = malloc(2*sizeof(char));
+  im->type = readLine(f);
+
+  im -> cn = 0;
+  getText(f, im);
+
+  //Size
+  fscanf (f, "%d", &im -> width);
+  fscanf (f, "%d", &im -> height);
+  fscanf (f, "%d", &im -> max);
+
+  //Pixels
+  getPixels(f, im);
+
+  return im;
+}
+
+/* Return PPM image i with message (hidden within r field) */
+struct PPM * encode (struct PPM * im, char * message, unsigned int mSize, unsigned intsecret){
+        srand(time(NULL)); //Randomize seed. Seed set to current time.
+        int i, width, sum, random;
+
+        width = im -> width;
+        sum = 0;
+
+        //Check for compatability
+        if((mSize * 75) < (width * im -> height)){
+          printf("steg: File is appropriate for encoding. \n");
+        }
+        else {
+          printf("steg: Error- Image size not acceptable. \n");
+          exit(0);
+        }
+
+        //for each char
+        for (i = 0; i < mSize;) {
+          random = (rand() % 100);
+          sum = sum + random;
+
+          //calc the pixel in terms of row and col
+		      int row, column;
+          row = sum / (width);
+          column = sum - (row * width);
+
+          //get pixel
+          struct pixel * pixel = &(im->pixels[row][column]); //Use memory address
+
+          //check red pixel val != asci of letter encoded
+          if(message[i] != pixel -> r){
+            pixel -> r = message[i];
+            i++;
+          }
+          else {
+            fprintf(stderr, "%s\n", "Retrying");
+          }
+  }
+  return i;
+}
+
+void writePPM(FILE * f, struct PPM * im, const char *fileName){
+	FILE *pfile = NULL;
+	int x,y;
+
+	output = fopen(fileName, "w");
+	fprintf(output, "P3\n%d %d\n255\n", i->w, i->h);
+
+	for(i = 0; i < im -> height; i++) {
+		for(j = 0; j < im -> width; j++){
+			struct pixel pixel = im -> pixels[x][y];
+			fprintf(output, "%i %i %i \n", pixel.r, pixel.g, pixel.b);		}
+	}
 	fclose(output);
 }
 
-struct PPM * encode(struct PPM * im, char * message, unsigned int mSize, unsigned int secret) {
 
-  srand(time(NULL)); // randomize seed being set to the current time for pixel placement.
+char * decode(struct PPM * i1, struct PPM * i2){
+	char * buffer = malloc(sizeof(char) * 255);
 
-  int i, random, width, sum;
+	//confirm that the files are the 'same'
+	if(!(i1->h == i2->h && i1->w == i2->w)){
+		printf("steg: Error - PPM files do not match. \n");
+		exit(0);
+	}
 
-  sum = 0;
-  width = im->header.width;
+	int j, k, l, m, n;
+	l = 0;
 
-  if((mSize * 75) < (width * im->header.height)){
-    printf("steg: File is appropriate for encoding. \n");
-  }
-  else {
-    printf("steg: Error- Image size not acceptable. \n");
-    exit(0);
-  }
+	//for each row
+	for(j=0;j<i2->h;j++){
+		//for each column
+		for(k=0;k<i2->w;k++){
+			//check if red pixels are not equal
+			if(i2->p[j][k].r != i1->p[j][k].r)
+				//get encoded char
+				buffer[l] = i2->p[j][k].r;
+				printf("%c", buffer[l]);
+				l = l+1;
+		}
+	}
 
-  //for each char
-  for (i=0;i<mSize;) {
-         random = (rand() % 100);
-         sum = sum + random;
-         int row, column;
-         row = sum / (width);
-         column = sum - (row * width);
+	//return txt
+	m = strlen(buffer) - 1;
+	if (buffer[m] == '\n')
+		buffer[m] = '\0';
 
-         //get pixel
-         struct PIXEL * p = &(im->pixels[row][column]); //Use memory address
+	char * str = malloc(sizeof(char) * (m+1));
 
-         //check red pixel val != asci of letter encoded
-         if(message[i] != p->r){
-           p->r = message[i];
-           i++;
-         }
-         else {
-           fprintf(stderr, "char [%i] - [%c] same. Retrying. \n", i, message[i]);
-        }
-  }
+	for(n=0; n<(m+1); n++)
+		str[n] = buffer[n];
 
-  return i;
+	free(buffer);
+	return str;
 
-};
+}
 
-/*
-char * decode(struct PPM * im, unsigned int secret) {
-
-};*/
 
 void encodeFile(int argc, char const ** argv){
-	char message[255];
-	int i;
+	char txt[255];
+	int j;
+	//FILE * outFin;
 
-	FILE * f = fopen(argv[2], "r");
-	if(f == NULL){
+	//outFin = fopen("modified.ppm", "w");
+
+	FILE * fin = fopen(argv[2], "r");
+	if(fin == NULL){
 		fprintf(stderr," steg: Error - Unable to open the file '%s' .\n", argv[2]);
 		exit(0);
 	}
 
-  PPM im = getPPM(f);
-	fprintf(stderr, "Message to Encode-> ");
-	fgets(message, 255, stdin);
+	struct PPM * i = getPPM(fin);
 
-	i = strlen(message) - 1;
-	if(message[i] == '\n')
-		message[i] = '\0';
+	//get txt
+	fprintf(stderr, "Message to Encode-> ");
+	fgets(txt, 255, stdin);
+
+	j = strlen(txt) - 1;
+	if(txt[j] == '\n')
+		txt[j] = '\0';
 
 	fprintf(stderr, "encoding in progress...\n");
 
 	//encode ppm
-  writePPM(f, &im);
-	showPPM(&im);
+	encode(txt, i);
+
+	//output ppm
+	//fputc(showPPM, outFin);
+	writePPM(fin, i, "modified.ppm");
+	showPPM(i);
 }
 
-/*
-  Main method which calls PPM functions for usage.
-*/
+void decodeFile(int argc, char const ** argv){
 
-int main(int argc, char *argv[]) {
-  FILE* f = fopen(argv[1], "r");
-  PPM ppm = getPPM(f);
-  fprintf(stderr, "%s\n", "Stenagography Program");
-  writePPM(f, &ppm);
-  showPPM(&ppm);
-  fclose(f);
-  return 0;
-};
+	FILE * i1 = fopen(argv[2], "r");
+	FILE * i2 = fopen(argv[3], "r");
+
+	if(i1 == NULL){
+		printf("steg: Error - File '%s' could not be opened. \n", argv[2]);
+		exit(0);
+	}
+
+	if(i2 == NULL){
+		printf("steg: Error - File '%s' could not be opened. \n", argv[3]);
+		exit(0);
+	}
+
+	//get files
+	struct PPM * oPPM = getPPM(i1);
+	struct PPM * ePPM = getPPM(i2);
+
+	//get encoded txt
+	char * str = decode(oPPM, ePPM);
+
+	//print txt
+	printf("%s\n", str);
+}
+
+int main (int argc, char const *argv[]){
+
+	//check argv
+	if(argc < 3){
+		printf("steg: Improper use of steg\n");
+		printf("Use either:  ./steg e <file1.ppm>\n");
+		printf("             ./steg d <file1.ppm> <file2.ppm>\n");
+		exit(0);
+	}
+
+	if(argv[1][0] == 'e'){
+		encodeFile(argc, argv);
+	}
+	else if(argv[1][0] == 'd'){
+		if(argc != 4){
+			printf("steg: Improper use of steg\n");
+                	printf("Use either:  ./steg e <file1.ppm>\n");
+                	printf("             ./steg d <file1.ppm> <file2.ppm>\n");
+                	exit(0);
+		}
+		decodeFile(argc, argv);
+	}
+	else{
+		printf("steg: Improper use of steg\n");
+                printf("Use either:  ./steg e <file1.ppm>\n");
+                printf("             ./steg d <file1.ppm> <file2.ppm>\n");
+                exit(0);
+	}
+
+	return 0;
+}
